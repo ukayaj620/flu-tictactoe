887e0457d09b1c2cda2afc177a319669442d5d8c
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2020-12-11 19:58:25 +0700

    feat: make home screen scrollable

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -33,74 +33,78 @@ class _HomeScreenState extends State<HomeScreen> {
         child: AppBar(),
       ),
       body: SafeArea(
-        child: Stack(
+        child: ListView(
           children: [
-            Positioned(
-              top: -1 / 9 * MediaQuery.of(context).size.height,
-              left: -1 / 5 * MediaQuery.of(context).size.width,
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: Image.asset('assets/vectors/signup_blob.png'),
-              ),
-            ),
-            Positioned(
-              top: 24.0,
-              left: 16.0,
-              child: Row(
-                children: [
-                  ProfileButton(
-                    onTap: () => Navigator.pushNamed(context, 'profile'),
+            Stack(
+              children: [
+                Positioned(
+                  top: -1 / 9 * MediaQuery.of(context).size.height,
+                  left: -1 / 5 * MediaQuery.of(context).size.width,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset('assets/vectors/signup_blob.png'),
                   ),
-                  Text(
-                    'Hi, ${_user.name}',
-                    style: Theme.of(context).textTheme.bodyText2,
+                ),
+                Positioned(
+                  top: 24.0,
+                  left: 16.0,
+                  child: Row(
+                    children: [
+                      ProfileButton(
+                        onTap: () => Navigator.pushNamed(context, 'profile'),
+                      ),
+                      Text(
+                        'Hi, ${_user.name}',
+                        style: Theme.of(context).textTheme.bodyText2,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 32.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  SizedBox(
-                    height: 1 / 4 * MediaQuery.of(context).size.height,
-                  ),
-                  Container(
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.symmetric(vertical: 16.0),
-                    child: Image.asset(
-                      'assets/icons/brand.png',
-                      width: MediaQuery.of(context).size.width * 0.5,
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 32.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        SizedBox(height: 32.0),
-                        Button(
-                          text: 'JOIN GAME',
-                          onPressed: () =>
-                              Navigator.pushNamed(context, 'join_game'),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 32.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                        height: 1 / 4 * MediaQuery.of(context).size.height,
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.symmetric(vertical: 16.0),
+                        child: Image.asset(
+                          'assets/icons/brand.png',
+                          width: MediaQuery.of(context).size.width * 0.5,
                         ),
-                        SizedBox(height: 24.0),
-                        Button(
-                          text: 'HOST GAME',
-                          onPressed: () => Navigator.pushNamed(context, 'host_game'),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(horizontal: 32.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            SizedBox(height: 32.0),
+                            Button(
+                              text: 'JOIN GAME',
+                              onPressed: () =>
+                                  Navigator.pushNamed(context, 'join_game'),
+                            ),
+                            SizedBox(height: 24.0),
+                            Button(
+                              text: 'HOST GAME',
+                              onPressed: () => Navigator.pushNamed(context, 'host_game'),
+                            ),
+                            SizedBox(height: 24.0),
+                            Button(
+                              text: 'ABOUT',
+                              onPressed: () =>
+                                  Navigator.pushNamed(context, 'about'),
+                            ),
+                          ],
                         ),
-                        SizedBox(height: 24.0),
-                        Button(
-                          text: 'ABOUT',
-                          onPressed: () =>
-                              Navigator.pushNamed(context, 'about'),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
